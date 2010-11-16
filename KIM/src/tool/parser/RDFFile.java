04501f103e934442d70a09e0fa09f49221e83f67
nkimkha@gmail.com <nkimkha@gmail.com@017abfc4-d696-6b33-ce71-229a49f329c2>
2010-11-16 02:18:05 +0000

    add test data

--- KIM/src/tool/parser/RDFFile.java
+++ KIM/src/tool/parser/RDFFile.java
@@ -3,27 +3,27 @@ package tool.parser;
 import java.io.File;
 import java.io.InputStream;
 
-import org.w3c.dom.Document;
-
-import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.ontology.Ontology;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.NodeIterator;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.util.FileManager;
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
 
 public class RDFFile {
 	private File file = null;
-	private Model model = null;
-	private NodeIterator list;
+	private OntModel model = null;
+	private ExtendedIterator<Ontology> list;
 	
 	public RDFFile(String filename) {
         try {
         	file = new File(filename);
         	if (file.exists()) {
-            	model = ModelFactory.createDefaultModel();
+            	model = ModelFactory.createOntologyModel(OntModelSpec.RDFS_MEM);
             	InputStream in = FileManager.get().open(filename);
             	model.read(in, null);
-            	list = model.listObjects();
+            	list = model.listOntologies();
         	}
 		} catch (Exception e) {
 			e.printStackTrace();
